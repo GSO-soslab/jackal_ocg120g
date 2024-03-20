61cc5039727c20e029783e94fd54c477c255589f
Lin Zhao <linzhao@uri.edu>
2024-03-20 16:50:55 -0400

    set laptop for different jackals

--- ocg120g_remote/setup/J2_laptop/setup_Demo2_J2.sh
+++ ocg120g_remote/setup/J2_laptop/setup_Demo2_J2.sh
@@ -2,13 +2,15 @@
 ###       in /etc/hosts for both jackals and laptops  ###
 ###       in order to connect to each others          ###
 
+### source remote workspace
+source /home/soslab/Develop/ros/jackal_ws/devel/setup.bash
 
 ### the host name of remote jackal name ###
 export ROS_MASTER_URI=http://cpr-j100-0608:11311
 
 ### the IP of current laptop to control the remote jackal ###
-export ROS_IP=192.168.1.178
+export ROS_IP=192.168.1.169
 
 ### launch the viz 
-roslaunch ocg120g_remote  visualize_demo1_J2.launch
+roslaunch ocg120g_remote visualize_demo2_J2.launch
 
