b691d37274f3793528fed956e4e3b6f8bddd1378
Lin Zhao <linzhao@uri.edu>
2025-02-04 13:38:22 -0500

    update for J3 demo1

--- ocg120g_remote/setup/J3_laptop/setup_J3_Demo1.sh
+++ ocg120g_remote/setup/J3_laptop/setup_J3_Demo1.sh
@@ -8,8 +8,8 @@
 ### the host name of remote jackal name ###
 export ROS_MASTER_URI=http://cpr-j100-0608:11311
 
-### the IP of current laptop to control the remote jackal ###
-export ROS_IP=192.168.1.178
+### Feild laptop (soslab-Latitude-5430-Rugged) to control the remote jackal ###
+export ROS_IP=192.168.1.173
 
 ### source the remote laptop ros workspace
 source /home/lin/develop/ros/jackal_ws/devel/setup.bash
