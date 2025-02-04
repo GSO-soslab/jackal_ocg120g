eb31aa46d5e52db731d22f519577495e4a99aefc
soslab <yweifu@uri.edu>
2025-02-04 17:23:51 -0500

    update for demo1 J1(livox)

--- ocg120g_remote/setup/J1_laptop/setup_J1_Demo1.sh
+++ ocg120g_remote/setup/J1_laptop/setup_J1_Demo1.sh
@@ -9,9 +9,11 @@ export ROS_MASTER_URI=http://cpr-j100-0551:11311
 ### the IP of current laptop to control the remote jackal ###
 export ROS_IP=192.168.1.165
 
-### source the remote ros workspace
-source /home/soslab/jackal_ws/devel/setup.bash
+### source the remote laptop ros workspace
+source /home/soslab/Develop/ros/jackal_ws/devel/setup.bash
 
+### launch the viz
+roslaunch ocg120g_remote visualize_demo1_J1.launch
 
 ### transmit dense pointcloud
 # rosparam set /draco_listener/point_cloud_transport draco
