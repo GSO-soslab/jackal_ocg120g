aa1cab4cb9c1cad5db94f53af0c7e2f6b32d9b9a
Lin Zhao <linzhao@uri.edu>
2025-04-01 18:27:02 -0400

    setup J3 with velodyne

--- ocg120g_remote/setup/J3_laptop/setup_J1_demo2.sh
+++ ocg120g_remote/setup/J3_laptop/setup_J1_demo2.sh
@@ -7,7 +7,7 @@
 export ROS_MASTER_URI=http://cpr-j100-0608:11311
 
 ### Field laptop (soslab-Latitude-5430-Rugged) to control the remote jackal ###
-export ROS_IP=192.168.1.173
+export ROS_IP=192.168.1.174
 
 ### source the remote laptop ros workspace
 source /home/soslab/Develop/ros/jackal_ws/devel/setup.bash
